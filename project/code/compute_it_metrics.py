0e990aeae088ffe4a628217b250c7a12dc65028f
Tiago Gonçalves <tiago.galves@hotmail.com>
2021-04-30 12:42:16 +0100

    Added all the plots to the project

--- project/code/compute_it_metrics.py
+++ project/code/compute_it_metrics.py
@@ -3,28 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+# Entropy
+from scipy.stats import entropy
+
+# Mutual Information
+from sklearn.feature_selection import mutual_info_classif
 
 
-# Function: Map input dims to output dims through a sliding window
-def map_inp_to_out_dims(input_arr, out_dim, offset):
-    # Create mapped_array
-    mapped_array = np.zeros((input_arr.shape[0], out_dim))
-
-    # Map spikes through a sliding window
-    _input_arr = input_arr.copy()[:, offset::]
-
-    # Go through
-    step = int((input_arr.shape[1] - offset) / out_dim)
-
-    for r in range(mapped_array.shape[0]):
-        tmp = _input_arr[r, :]
-        
-        init_idx = 0
-        for c in range(out_dim):
-            mapped_array[r, c] = 1 if np.mean(tmp[init_idx:init_idx+step]) > 0 else 0
-            init_idx += step
-
-    return mapped_array
 
 # Global Variables
 time = 250
@@ -69,22 +54,52 @@ for label, files in enumerate(zip(batches, spikes)):
     # print(spike.shape)
 
 
-    # Perform a sliding window to map input spikes to output spikes dimensions
-    b_mapped = map_inp_to_out_dims(input_arr=batch, out_dim=spike.shape[1], offset=4)
-
     # Marginal probabilities per mapped neuron
     # Col-0 prob(x=0), Col-1 prob(x=1)
-    b_mapped_probs = np.zeros((b_mapped.shape[1], 2))
-    for neuron in range(b_mapped.shape[1]):
-        b_mapped_probs[neuron, 1] = np.sum(b_mapped[:, neuron]) / time
-        b_mapped_probs[neuron, 0] = 1 - (np.sum(b_mapped[:, neuron]) / time)
-        # print(np.sum(b_mapped_probs[neuron, :]))
+    batch_probs = np.zeros((batch.shape[1], 2))
+    for neuron in range(batch.shape[1]):
+        batch_probs[neuron, 1] = np.sum(batch[:, neuron]) / time
+        batch_probs[neuron, 0] = 1. - (np.sum(batch[:, neuron]) / time)
+        # print(np.sum(batch_probs[neuron, :]))
+    # print(batch_probs)
+    
 
+    # Compute Entropy Input Neurons
+    in_neurons_entropy = np.zeros((batch_probs.shape[0], ))
+    for neuron in range(batch_probs.shape[0]):
+        entropy_value = entropy(pk=[batch_probs[neuron, 0], batch_probs[neuron, 1]], base=2)
+        in_neurons_entropy[neuron] = entropy_value
     
     # Marginal probabilities per spike neuron
     # Col-0 prob(x=0), Col-1 prob(x=1)
-    s_probs = np.zeros_like(b_mapped_probs)
+    s_probs = np.zeros((spike.shape[1], 2))
     for neuron in range(spike.shape[1]):
         s_probs[neuron, 1] = np.sum(spike[:, neuron]) / time
-        s_probs[neuron, 0] = 1 - (np.sum(spike[:, neuron]) / time)
+        s_probs[neuron, 0] = 1. - (np.sum(spike[:, neuron]) / time)
         # print(np.sum(s_probs[neuron, :]))
+    # print(s_probs)
+    
+    # Compute Entropy Output Neurons
+    out_neurons_entropy = np.zeros((s_probs.shape[0], ))
+    for neuron in range(s_probs.shape[0]):
+        entropy_value = entropy(pk=[s_probs[neuron, 0], s_probs[neuron, 1]], base=2)
+        out_neurons_entropy[neuron] = entropy_value
+    
+
+    # Debug prints (Entropies)
+    # print(list(in_neurons_entropy))
+    # print(list(out_neurons_entropy))
+
+# Compute MI
+mi_array = np.zeros((spike.shape[1], batch.shape[1]))
+
+for i in range(spike.shape[1]):
+
+    print(i)
+    mi = mutual_info_classif(X=np.array(batch, dtype=np.int), y=np.array(spike[:, i], dtype=np.int), discrete_features=True)
+    mi_array[i] = mi
+    # print(mi)
+
+for i in range(mi_array.shape[0]):
+    if np.sum(mi_array[i, :]) > 0:
+        print(i, np.sum(mi_array[i, :]))
---
+++ project/code/create_figures.py
@@ -0,0 +1,94 @@
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+# Global Variables
+time = 250
+
+
+# Directories
+results = "results/predictions"
+datasets = ["MNIST", "FashionMNIST", "CIFAR10"]
+
+# Go through datasets
+for dataset in datasets:
+    dataset_plot_dir = os.path.join("results", "figures", dataset)
+
+    if os.path.isdir(dataset_plot_dir) == False:
+        os.makedirs(dataset_plot_dir)
+
+    # Open directory
+    files = os.listdir(os.path.join(results, dataset))
+    files = [i for i in files if not i.startswith('.')]
+    files.sort()
+    # print(files)
+
+    # Get batches
+    batches = [f for f in files if f.startswith('b')]
+    batches.sort()
+    # print(batches)
+
+    # Get spikes
+    spikes = [f for f in files if f.startswith('s')]
+    spikes.sort()
+    # print(spikes)
+
+
+    # Go through pairs
+    for label, files in enumerate(zip(batches, spikes)):
+        # Get batch and files
+        b, s = files
+        # print(b, s)
+
+        # Load batch
+        batch = np.load(file=os.path.join(results, dataset, b), allow_pickle=True)
+        batch = np.reshape(a=batch, newshape=(time, -1))
+        # print(batch.shape)
+
+
+        # Create some good plots
+        x = list()
+        y = list()
+        for t in range(batch.shape[0]):
+            for n_idx in batch[t, :].nonzero()[0]:
+                # print(n_idx)
+                x.append(t)
+                y.append(n_idx)
+
+        # print(len(x), len(y))
+        plt.title(f"Input Spikes | Label {label}")
+        plt.xlim(-50, 300)
+        plt.xlabel("Time (ms)")
+        plt.ylim(0, batch.shape[1]+10)
+        plt.ylabel("Neuron Index")
+        plt.scatter(x, y)
+        plt.savefig(fname=os.path.join(dataset_plot_dir, f"in_spikes_{label}.png"), bbox_inches="tight", pad_inches=0.0)
+        plt.show()
+
+
+
+
+        # Load spike
+        spike = np.load(file=os.path.join(results, dataset, s), allow_pickle=True)
+        spike = np.reshape(a=spike, newshape=(time, -1))
+        # print(spike.shape)
+
+        # Create some good plots
+        x = list()
+        y = list()
+        for t in range(spike.shape[0]):
+            for n_idx in spike[t, :].nonzero()[0]:
+                # print(n_idx)
+                x.append(t)
+                y.append(n_idx)
+
+        # print(len(x), len(y))
+        plt.title(f"Output Spikes | Label {label}")
+        plt.xlim(-50, 300)
+        plt.xlabel("Time (ms)")
+        plt.ylim(0, spike.shape[1]+10)
+        plt.ylabel("Neuron Index")
+        plt.scatter(x, y)
+        plt.savefig(fname=os.path.join(dataset_plot_dir, f"out_spikes_{label}.png"), bbox_inches="tight", pad_inches=0.0)
+        plt.show()
